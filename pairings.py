275f43d047a3db00a3010f614783af59e1128b18
Laura Castro <lauracastrotech@gmail.com>
2025-06-13 13:22:58 -0400

    solves problem

--- pairings.py
+++ pairings.py
@@ -1,6 +1,27 @@
 def find_pairs(num_string):
-  # Write your solution here!
-  pass
+  # Split the string into list of positive nubmers
+  # Convert each number to an int
+  # Loop through my list compare the current value with remaining items by looping through the list as well
+  # Create two value tuple based on the comparison
+  # Add the tuple to the set
+  # return the set
+  pairs = set()
+
+  if not len(num_string) > 1:
+    return pairs
+  
+  list_of_nums = num_string.split(' ')
+  list_of_ints = [int(num) for num in list_of_nums]
+  # print(f'{list_of_nums=}')
+  # print(f'{list_of_ints=}')
+  for int_num in list_of_ints:
+    num_tup = ()
+    for next_int in list_of_ints:
+      if int_num < next_int:
+        num_tup = (int_num, next_int)
+        pairs.add(num_tup)
+  
+  return pairs
 
 
 # Test cases
